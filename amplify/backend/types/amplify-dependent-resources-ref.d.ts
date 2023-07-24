69249232f22dc185935f5b5d51d58338564fc56b
Peter Trost <trost@intervall.io>
2023-07-24 16:38:13 +0200

    Add test for DB access

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,41 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apidc09751e": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "coveragereportroamera134eff3a134eff3": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "coveragereportroamer0bdec695": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamof553b790": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
     }
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Amplify } from "aws-amplify";
+import React, { useEffect } from "react";
+import { Amplify, API } from "aws-amplify";
 
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
@@ -7,6 +8,63 @@ import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
+  useEffect(() => {
+    const apiName = "apidc09751e";
+    const path = "/items";
+    const getObj = {
+      headers: {}, // OPTIONAL
+      response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+      queryStringParameters: {
+        name: "Peter", // OPTIONAL
+        age: 29, // OPTIONAL
+      },
+    };
+    const postObj = {
+      headers: {}, // OPTIONAL
+      response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+      body: {
+        name: "Peter",
+        age: 29,
+      },
+    };
+
+    // Test GET method
+    API.get(apiName, path + "/object", getObj)
+      .then((response) => {
+        console.log("GET response:", response);
+      })
+      .catch((error) => {
+        console.log("GET error:", error);
+      });
+
+    // Test POST method
+    API.post(apiName, path, postObj)
+      .then((response) => {
+        console.log("POST response:", response);
+      })
+      .catch((error) => {
+        console.log("POST error:", error);
+      });
+
+    // // Test PUT method
+    // API.put(apiName, path, myInit)
+    //   .then((response) => {
+    //     console.log("PUT response:", response);
+    //   })
+    //   .catch((error) => {
+    //     console.log("PUT error:", error);
+    //   });
+
+    // // Test DELETE method
+    // API.del(apiName, path, myInit)
+    //   .then((response) => {
+    //     console.log("DELETE response:", response);
+    //   })
+    //   .catch((error) => {
+    //     console.log("DELETE error:", error);
+    //   });
+  }, []);
+
   return (
     <>
       <h1>Hello {user.username}</h1>
